eecd690bab6a22d00b9bb10d0df09c6a50a08041
YashCompro <yash.aggarwal@comprotechnologies.com>
2021-01-14 13:34:04 +0530

    added dialog v404

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
